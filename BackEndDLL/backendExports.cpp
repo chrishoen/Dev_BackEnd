884f2b4d428e31fdda7dd63a9be6e94325e16a37
chrishoen <chris123hoen@gmail.com>
2019-03-19 18:48:14 -0400

    added project BackEndDLL

---
+++ BackEndDLL/backendExports.cpp
@@ -0,0 +1,98 @@
+
+#include "stdafx.h"
+
+#include "MainInit.h"
+#include "ProcInit.h"
+#include "backendInterfaceThread.h"
+
+#include "backendExports.h"
+namespace BackEnd
+{
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Regional variables.
+
+bool mValidFlag = false;
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Initialize the dll. Read a settings file, initialize resources, and
+// launch threads. This must be called before any other functions in
+// the dll can be called. Return true if successful.
+// 
+// The settings file contains sections of commands that are used to 
+// set dll settings variables.
+// 
+// A filepath for the settings file and a section within the settings
+// file are supplied as inputs.
+// 
+// First this reads the default section of the file and then reads the
+// input section. This allows having default settings with overrides
+// for some specific variables.
+
+bool DllExport initializeBackEnd()
+{
+   // If already ininialized then exit.
+   if (mValidFlag) return false;
+
+   // Set invalid.
+   mValidFlag = false;
+
+   // Initialize resources.
+   main_initialize();
+
+   // Initialize threads.
+   proc_initialize();
+
+   // Set valid.
+   mValidFlag = true;
+
+   return true;
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Finalize  the dll. Finalize resources and terminate threads. This
+// must be called before the process termination section force
+// terminates any running threads. Return true if successful.
+
+bool DllExport finalizeBackEnd()
+{
+   // Guard.
+   if (!mValidFlag) return false;
+
+   // Finalize resources.
+   proc_finalize();
+
+   // Finalize threads.
+   main_finalize();
+
+   // Set invalid.
+   mValidFlag = false;
+
+   return true;
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Get the current status. This is passed in a status record which
+// is filled with the current status. The record must first be
+// created by the caller. 
+
+int DllExport getBackEndStatus()
+{
+   // Guard.
+   if (!mValidFlag) return -1;
+
+   return 101;
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+}//namespace
---
+++ BackEndDLL/backendExports.h
@@ -0,0 +1,54 @@
+#pragma once
+/*==============================================================================
+Export definitions for the backed DLL. 
+==============================================================================*/
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+#define DllExport   __declspec(dllexport) __stdcall
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+#ifdef __cplusplus
+extern "C" {
+#endif
+  
+namespace BackEnd
+{
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Initialize the dll. Read a settings file, initialize resources, and
+// launch threads. This must be called before any other functions in
+// the dll can be called. Return true if successful.
+// 
+
+bool DllExport initializeBackEnd();
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Finalize  the dll. Finalize resources and terminate threads. This
+// must be called before the process termination section force
+// terminates any running threads. Return true if successful.
+
+bool DllExport finalizeBackEnd();
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Get the current status. This is passed in a status record which
+// is filled with the current status. The record must first be
+// created by the caller. 
+
+int DllExport getBackEndStatus();
+
+
+}//namespace 
+
+#ifdef __cplusplus
+}
+#endif
+
