ecf17336039993e18ff76836fd9d331c82bb5079
chrishoen <chris123hoen@gmail.com>
2019-04-16 22:28:41 -0400

    backend lib uses rislib

--- BackEndLib/backendExports.cpp
+++ BackEndLib/backendExports.cpp
@@ -1,6 +1,10 @@
 
 #include "stdafx.h"
 
+#include "MainInit.h"
+#include "ProcInit.h"
+#include "backendInterfaceThread.h"
+
 #include "backendExports.h"
 namespace BackEnd
 {
@@ -16,11 +20,87 @@ int  mCount = 0;
 //******************************************************************************
 //******************************************************************************
 //******************************************************************************
+// Initialize the dll. Read a settings file, initialize resources, and
+// launch threads. This must be called before any other functions in
+// the dll can be called. Return true if successful.
+// 
+// The settings file contains sections of commands that are used to 
+// set dll settings variables.
+// 
+// A filepath for the settings file and a section within the settings
+// file are supplied as inputs.
+// 
+// First this reads the default section of the file and then reads the
+// input section. This allows having default settings with overrides
+// for some specific variables.
+
+bool  initializeBackEnd()
+{
+   // If already ininialized then exit.
+   if (mValidFlag) return false;
+
+   // Set invalid.
+   mValidFlag = false;
+
+   // Initialize resources.
+   main_initialize();
+
+   // Initialize threads.
+   proc_initialize();
+
+   // Set valid.
+   mValidFlag = true;
+
+   return true;
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Finalize  the dll. Finalize resources and terminate threads. This
+// must be called before the process termination section force
+// terminates any running threads. Return true if successful.
+
+bool  finalizeBackEnd()
+{
+   // Guard.
+   if (!mValidFlag) return false;
+
+   // Finalize resources.
+   proc_finalize();
+
+   // Finalize threads.
+   main_finalize();
+
+   // Set invalid.
+   mValidFlag = false;
+
+   return true;
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Get the current status. This is passed in a status record which
+// is filled with the current status. The record must first be
+// created by the caller. 
+
+int  getBackEndStatus()
+{
+   // Guard.
+   if (!mValidFlag) return -1;
+
+   return 101;
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
 // Set a string.
 
 void  setMyString(char* aString)
 {
-   printf("setMyString %s\n", aString);
+   Prn::print(Prn::View21, "setMyString %s", aString);
 }
 
 //******************************************************************************
@@ -30,20 +110,20 @@ void  setMyString(char* aString)
 
 void  getMyString(char* aString)
 {
-   printf("getMyString\n");
+   Prn::print(Prn::View21, "getMyString");
    strcpy(aString, "ABCDEFGH");
 }
 
 int   getCount()
 {
-   printf("getCount %d\n",mCount);
+   Prn::print(Prn::View21, "getCount %d",mCount);
    return mCount++;
 }
 
 void  setCount(int aCount)
 {
    mCount = aCount;
-   printf("setCount %d\n", mCount);
+   Prn::print(Prn::View21, "setCount %d", mCount);
 }
 
 //******************************************************************************
--- BackEndLib/stdafx.h
+++ BackEndLib/stdafx.h
@@ -18,3 +18,8 @@
 #include <vector>
 #include <iterator>
 
+#include "my_functions.h"
+#include "prnPrint.h"
+#include "logFiles.h"
+#include "risPortableCalls.h"
+
