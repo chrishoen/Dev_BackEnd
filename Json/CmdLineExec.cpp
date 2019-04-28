2dc1824be6faade8498623b42ead81a01bc756ab
chrishoen <chris123hoen@gmail.com>
2019-04-28 12:01:16 -0400

    more json

--- Json/CmdLineExec.cpp
+++ Json/CmdLineExec.cpp
@@ -33,12 +33,16 @@ void CmdLineExec::reset()
 
 void CmdLineExec::execute(Ris::CmdLineCmd* aCmd)
 {
-   if(aCmd->isCmd("RESET"  ))  reset();
-   if(aCmd->isCmd("GO1"    ))  executeGo1(aCmd);
-   if(aCmd->isCmd("GO2"    ))  executeGo2(aCmd);
-   if(aCmd->isCmd("GO3"    ))  executeGo3(aCmd);
-   if(aCmd->isCmd("GO4"    ))  executeGo4(aCmd);
-   if(aCmd->isCmd("GO5"    ))  executeGo5(aCmd);
+   if (aCmd->isCmd("RESET"))   reset();
+   if (aCmd->isCmd("GO1"))     executeGo1(aCmd);
+   if (aCmd->isCmd("GO2"))     executeGo2(aCmd);
+   if (aCmd->isCmd("GO3"))     executeGo3(aCmd);
+   if (aCmd->isCmd("GO4"))     executeGo4(aCmd);
+   if (aCmd->isCmd("GO5"))     executeGo5(aCmd);
+   if (aCmd->isCmd("GO6"))     executeGo6(aCmd);
+   if (aCmd->isCmd("GO7"))     executeGo7(aCmd);
+   if (aCmd->isCmd("GO8"))     executeGo8(aCmd);
+   if (aCmd->isCmd("GO9"))     executeGo9(aCmd);
 }
 
 //******************************************************************************
@@ -127,6 +131,61 @@ void CmdLineExec::executeGo4(Ris::CmdLineCmd* aCmd)
 //******************************************************************************
 
 void CmdLineExec::executeGo5(Ris::CmdLineCmd* aCmd)
+{
+   std::stringstream tStream1;
+   std::stringstream tStream2;
+   std::string tString;
+   Json::Value tRoot1;
+   Json::Value tRoot2;
+
+   std::cout << "LINE101" << std::endl;
+   tRoot1["msg-id"] = "test4_msg";
+   tRoot1["payload"] = "some_payload";
+
+   std::cout << "LINE102" << std::endl;
+   tStream1 << tRoot1 << std::endl;
+   tString = tStream1.str();
+   std::cout << tString;
+
+   std::cout << "LINE103" << std::endl;
+   tStream2.str(tString);
+   std::cout << tStream2.str();
+
+   std::cout << "LINE104" << std::endl;
+   tStream2 >> tRoot2;
+   std::cout << tRoot2;
+   std::cout << "LINE105" << std::endl;
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeGo6(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeGo7(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeGo8(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeGo9(Ris::CmdLineCmd* aCmd)
 {
    Prn::print(Prn::View11, "TESTING11");
    Prn::print(Prn::View21, "TESTING21");
